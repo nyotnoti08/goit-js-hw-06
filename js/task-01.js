293f14feebd1161d7917c31243fe7b3d36862a28
nyotnoti08 <50961779+nyotnoti08@users.noreply.github.com>
2023-11-10 05:39:55 +0800

    Update task-01.js
    Change console.log() result from string to string with a number

--- js/task-01.js
+++ js/task-01.js
@@ -3,15 +3,15 @@ const categoryItems = categoriesList.querySelectorAll(".item");
 
 let categoryCount = 0;
 
-console.log("Number of categories: " + categoryItems.length);
+console.log("Number of categories: " , categoryItems.length);
 console.log(" ");
 
 categoryItems.forEach((item) => {
   const categoryHeader = item.querySelector("h2");
   const categoryElements = item.querySelectorAll("ul > li");
 
-  console.log("Category: " + categoryHeader.textContent);
-  console.log("Elements: " + categoryElements.length);
+  console.log("Category: " , categoryHeader.textContent);
+  console.log("Elements: " , categoryElements.length);
   console.log(" ");
 });
 
